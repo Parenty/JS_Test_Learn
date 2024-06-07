afd644711d33593f0fa06b2420657d61503f38f6
Parenty <Realdeathstroke@yandex.ru>
2024-06-07 18:08:31 +0300

    add asserts

--- tests/capitalize.test.js
+++ tests/capitalize.test.js
@@ -2,13 +2,10 @@
 // попробуйте изменять код функции / тестов, запуская проверки заново
 
 import { capitalize } from '../src/capitalize.js';
+import { strict as assert } from 'node:assert';
 
-if (capitalize('hello') !== 'Hello') {
-    throw new Error('Функция работает неверно!');
-}
+assert.strictEqual(capitalize('hello'), 'Hello');
 
-if (capitalize('') !== '') {
-    throw new Error('Функция работает неверно!');
-}
+assert.strictEqual(capitalize(''), '');
 
 console.log('Все тесты пройдены!');
